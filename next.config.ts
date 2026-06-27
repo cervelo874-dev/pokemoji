a417e98f081f0cc8bba98ceedc6d47fcc6906192
cervelo874-dev <cervelo874@gmail.com>
2026-06-27 21:51:58 +0900

    chore: dynamically set basePath for GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+// Check if running on GitHub Actions to set appropriate basePath for GitHub Pages
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isGithubActions ? '/pokemoji' : '',
 };
 
 export default nextConfig;
